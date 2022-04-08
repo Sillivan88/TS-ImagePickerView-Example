8e65f3cb1c5aba2bfd21737c63c692ac2baa06ac
Thomas Sillmann <contact@thomassillmann.de>
2022-04-08 09:32:08 +0200

    Initial Commit

---
+++ TS ImagePickerView Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TS ImagePickerView Example
+//
+//  Created by Thomas Sillmann on 08.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TS ImagePickerView Example/TS_ImagePickerView_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TS_ImagePickerView_ExampleApp.swift
+//  TS ImagePickerView Example
+//
+//  Created by Thomas Sillmann on 08.04.22.
+//
+
+import SwiftUI
+
+@main
+struct TS_ImagePickerView_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
